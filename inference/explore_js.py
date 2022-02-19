b2e72bee0de0e053cbc7b5416774e5fcfc7422c0
Jamie-Stirling <stirlingj00@gmail.com>
2022-02-19 21:19:52 +0000

    grouped mean/std

--- inference/explore_js.py
+++ inference/explore_js.py
@@ -6,14 +6,25 @@ prices = []
 dates = []
 
 group_model = {"DH1":(2321,2214)}
+groups = {}
 
 for years in [2018, 2019, 2020, 2021]:
     with open("inference/data/pp-{}.csv".format(years)) as r:
         lines = [l for l in r.readlines()]
         np.random.shuffle(lines)
-        for line in lines[:2000]:
+        for line in lines[:20000]:
             spl = line.split(",")
             prices.append(float(spl[1][1:-1]))
             dates.append(datetime.strptime(spl[2][1:-1], "%Y-%m-%d %H:%M"))
+            postcode = spl[3][1:-1]
 
-prices = np.array(prices)
+            if postcode != "":
+                firsthalf = postcode.split(" ")[0]
+                if firsthalf not in groups:
+                    groups[firsthalf] = []
+                groups[firsthalf].append(prices[-1])
+
+for key in groups:
+    group_model[key] = (np.mean(groups[key]), np.std(groups[key]))
+
+print(group_model)
